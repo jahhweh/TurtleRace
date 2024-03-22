b987796dfb34f3ecce10c81e47c54526089580ef
jahhweh <zacheidman@gmail.com>
2024-03-22 18:27:07 -0400

    commit

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 // Turtle Race!
-// !race to start a turtle race
+// by @jahhweh
 
 import * as dotenv from 'dotenv';
 import { OpenAI } from 'openai';
 import { Client } from 'discord.js';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  increment,
+  arrayUnion
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 dotenv.config();
 const client = new Client({
